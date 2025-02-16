4a579c4ea41e5c3423d1f80577241cf002eb6b67
bedirhangull <gu1bedirhann@gmail.com>
2025-02-16 16:28:37 +0300

    toaster added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Analytics } from "@vercel/analytics/react"
+import { Toaster } from "@/components/ui/toaster"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
       <GoogleAnalytics gaId="G-9Z97CMZWRH" />
       <Analytics />
